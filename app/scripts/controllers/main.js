99b15ca9fb4143849fe2654bf47a5360b07ec940
Justin <lopez@apollointeractive.com>
2014-06-04 18:19:18 -0700

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('flashcardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
